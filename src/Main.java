a75a8c97dfbdeb49559366d471a0f9a5d9c2f257
MrLagSux <paul.haupt@gmx.net>
2023-04-12 23:00:06 +0200

    Add flag parser
    Added prototype of parsing CLI flags. For now only `input` is used.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String, String> parsedFlags = Flag.parseFlags(args);
+        System.out.println(parsedFlags);
     }
 }
---
+++ src/util/Utility.java
@@ -0,0 +1,18 @@
+package util;
+
+public class Utility {
+    public static void printCompilerError(String error) {
+        System.err.println("ERROR: " + error);
+        System.exit(1);
+    }
+
+    public static void printCompilerErrorWithMessage(String error, String info) {
+        System.err.println("ERROR: " + error + "\n" + info);
+        System.exit(3);
+    }
+
+    public static void printNotImplementedError(String msg) {
+        System.err.println("Oops! I have not implemented " + msg + " yet.");
+        System.exit(2);
+    }
+}
