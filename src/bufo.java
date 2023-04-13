144704145838fa96b8d1f68e6494eaf79651bc9d
MrLagSux <paul.haupt@gmx.net>
2023-04-13 11:00:28 +0200

    Rename Main.java to bufo.java

--- src/bufo.java
+++ src/bufo.java
@@ -3,7 +3,7 @@ import heart.Flag;
 
 import java.util.HashMap;
 
-public class Main {
+public class bufo {
     public static void main(String[] args) {
         HashMap<String, String> parsedFlags = Flag.parseFlags(args);
         Compiler.initialize(parsedFlags);
