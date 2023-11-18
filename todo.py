0f9e1853831c3a85f5959cd878a44f7980713157
pfhaupt <pfhaupt@gmx.net>
2023-11-18 20:55:53 +0100

    Write small helper script for hunting TODOs

---
+++ todo.py
@@ -0,0 +1,14 @@
+import subprocess
+from typing import List
+import random
+
+def call_cmd(cmd: List) -> int:
+    return subprocess.check_output(cmd).split(b'\n')
+
+def get_random():
+    todo = call_cmd(["git", "grep", "-nE", "(todo!|TODO|FIXME)"])
+    if len(todo) == 0:
+        return "DONE! No todo!(), TODO or FIXME found!"
+    return str(random.choice(todo))
+
+print(get_random())
