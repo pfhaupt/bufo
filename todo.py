0f8c2062e1539896c509171cd903f61dfb17b341
pfhaupt <pfhaupt@gmx.net>
2023-11-18 23:03:33 +0100

    Fix todo.py
    It successfully recommended itself :^)
    Obviously we do not want that for fix-hunting.

--- todo.py
+++ todo.py
@@ -6,7 +6,8 @@ def call_cmd(cmd: List) -> int:
     return subprocess.check_output(cmd).split(b'\n')
 
 def get_random():
-    todo = call_cmd(["git", "grep", "-nE", "(todo!|TODO|FIXME)"])
+    todo = call_cmd(["git", "grep", "-nE", "(todo!|TODO|FIXME)", "--", ":!todo.py"])
+    print(f"Found {len(todo)} things that need fixing!")
     if len(todo) == 0:
         return "DONE! No todo!(), TODO or FIXME found!"
     return str(random.choice(todo))
