7fc67169b0f2d2bfdf1b0e66429aa6c29b4ba2f7
pfhaupt <pfhaupt@gmx.net>
2023-11-18 01:36:55 +0100

    Add helper for getting register name

--- src/new/new_assembler.rs
+++ src/new/new_assembler.rs
@@ -1,5 +1,27 @@
 
 use crate::new::instr;
+use crate::new::instr::OperandType;
+
+use super::instr::{Register, RegMode};
+
+const REG_64BIT: [&str; 16] = [
+    "rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15",
+];
+const REG_32BIT: [&str; 16] = [
+    "eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi", "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d",
+];
+
+fn reg(r: Register, rm: RegMode) -> &'static str {
+    let index = r as usize;
+    debug_assert!(index < REG_32BIT.len());
+    if rm == RegMode::BIT32 {
+        REG_32BIT[index]
+    } else if rm == RegMode::BIT64 {
+        REG_64BIT[index]
+    } else {
+        unreachable!()
+    }
+}
 
 pub struct Assembler {
     print_debug: bool,
