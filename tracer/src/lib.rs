dc8748ba3d7e2dbc7bf8bbae8018d0a14b8a2038
pfhaupt <pfhaupt@gmx.net>
2023-12-01 20:06:22 +0100

    Tracer: Import `syn` and `quote` crates
    I'm usually not a fan of 3rd party tools, but they'll pass. Procedural
    macros are hard, and they allow me to do exactly what I want.

--- tracer/src/lib.rs
+++ tracer/src/lib.rs
@@ -1,83 +1,54 @@
 extern crate proc_macro;
 use proc_macro::*;
+use quote::quote;
 
 #[proc_macro_attribute]
 pub fn trace_call(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // item is the function to be injected
     // it adds a println!() to the beginning of the function that prints the function name
     debug_assert!(item.to_string().contains("fn"));
-    let item_str = item.to_string();
-    let fn_pub = if item_str.contains("pub") { "pub " } else { "" };
-    let fn_name = item_str
-        .split("fn")
-        .nth(1)
-        .unwrap()
-        .split("(")
-        .nth(0)
-        .unwrap()
-        .trim()
-        .to_string();
-    println!("[LOG] Injecting log into function `{}`", fn_name);
+    let func = syn::parse_macro_input!(item as syn::ItemFn);
 
-    let fn_args = item_str
-        .splitn(2, "(")
-        .nth(1)
-        .unwrap()
-        .splitn(2, ")")
-        .nth(0)
-        .unwrap()
-        .trim()
-        .to_string();
+    let syn::ItemFn {
+        attrs,
+        vis,
+        sig,
+        block,
+    } = func;
+    let name = sig.ident.to_string();
 
-    let fn_return = if item.to_string().contains("->") {
-        "->".to_owned()
-            + &item_str
-                .splitn(2, "->")
-                .nth(1)
-                .unwrap()
-                .splitn(2, "{")
-                .nth(0)
-                .unwrap()
-                .trim()
-                .to_string()
+    let block = if attrs.iter().any(|attr| attr.path.is_ident("track_caller")) {
+        quote! { #block }
     } else {
-        "".to_string()
+        quote! {
+            (move || #block )()
+        }
     };
 
-    let fn_body = item_str
-        .splitn(2, "{")
-        .nth(1)
-        .unwrap()
-        .rsplitn(2, "}")
-        .nth(1)
-        .unwrap()
-        .trim()
-        .to_string();
+    let traced_output = quote! {
+        #[track_caller]
+        #(#attrs)*
+        #vis #sig {
+            println!("[LOG] {}: Entering `{}`",
+                ::core::panic::Location::caller(),
+                #name,
+            );
+            #block
+        }
+    };
+    let normal_output = quote! {
+        #(#attrs)*
+        #vis #sig {
+            #block
+        }
+    };
 
-    // Only inject the tracing code if the feature is enabled
-    let tracing_fn = format!(
-        "
-        {} fn {}({}) {} {{
-            println!(\"[LOG] {{}}:{{}}: {}() called\", file!(), line!());
-            {}
-        }}",
-        fn_pub, fn_name, fn_args, fn_return, fn_name, fn_body
-    );
-    let normal_fn = format!(
-        "
-        {} fn {}({}) {} {{
-            {}
-        }}",
-        fn_pub, fn_name, fn_args, fn_return, fn_body
+    let output = quote!(
+        #[cfg(feature = "trace")]
+        #traced_output
+        #[cfg(not(feature = "trace"))]
+        #normal_output
     );
 
-    let feature_fn = format!(
-        "#[cfg(feature = \"trace\")]
-        {}
-        #[cfg(not(feature = \"trace\"))]
-        {}",
-        tracing_fn, normal_fn
-    );
-
-    feature_fn.parse().unwrap()
+    output.into()
 }
